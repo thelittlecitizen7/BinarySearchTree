6fab2895ed5a669a078ec243f95e0a8bf52e3ac0
thelittlecitizen7 <thelittlecitizen7@gmail.com>
2020-08-13 22:21:34 +0300

    finish all tree logic

---
+++ BinarySearchTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        public BSTNode Root { get; set; }
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public BinarySearchTree(int value)
+        {
+            Root = new BSTNode(value);
+        }
+
+        public BinarySearchTree(BSTNode root)
+        {
+            Root = root;
+        }
+
+        public int GetMax()
+        {
+            return Root.Max();
+        }
+
+        public int GetMin()
+        {
+            return Root.Min();
+        }
+
+        public bool AddItem(int value)
+        {
+            return Root.AddItem(value);
+        }
+
+        public bool Contains(int value)
+        {
+            return FindItem(value) != null;
+        }
+
+        public BSTNode FindItem(int value)
+        {
+            if (Root == null)
+            {
+                return null;
+            }
+            return Root.FindItem(value);
+        }
+
+        public bool DeleteItem(int value)
+        {
+            if (Root == null)
+            {
+                return false;
+            }
+            return Root.DeleteItem(value, Root);
+        }
+
+        private void OrderBst()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private BSTNode SortedArrayToBST(int[] array, int start, int end)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private IEnumerable<int> Scan(BSTNode node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BinarySearchTreeNode.cs
@@ -0,0 +1,223 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BinarySearchTree
+{
+    public class BSTNode
+    {
+        public BSTNode(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        public BSTNode Left { get; set; }
+
+        public BSTNode Right { get; set; }
+
+        public int Min()
+        {
+            if (Left != null)
+            {
+                return Left.Min();
+            }
+            return Value;
+        }
+
+
+
+        public int Max()
+        {
+            if (Right != null)
+            {
+                return Right.Max();
+            }
+            return Value;
+        }
+
+
+        public bool AddItem(int value)
+        {
+            if (Value < value)
+            {
+                if (Right == null)
+                {
+                    Right = new BSTNode(value);
+                    return true;
+                }
+                else
+                {
+                    return Right.AddItem(value);
+                }
+            }
+            else if (Value > value)
+            {
+                if (Left == null)
+                {
+                    Left = new BSTNode(value);
+                    return true;
+                }
+                else
+                {
+                    return Left.AddItem(value);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool Contains(int value)
+        {
+            if (Value == value)
+            {
+                return true;
+            }
+            BSTNode startNode = null;
+            if (Right.Value > value)
+            {
+                startNode = Right;
+            }
+            else
+            {
+                startNode = Left;
+            }
+            BSTNode node = FindByRecursive(startNode, value);
+            return (node != null);
+        }
+
+
+
+        public BSTNode FindItem(int value)
+        {
+            if (Value == value)
+            {
+                return this;
+            }
+            BSTNode startNode = null;
+            if (Right != null)
+            {
+                if (value >= Value)
+                {
+                    startNode = Right;
+                }
+
+            }
+            if (Left != null)
+            {
+                if (value <= Left.Value)
+                {
+                    startNode = Left;
+                }
+            }
+            BSTNode node = FindByRecursive(startNode, value);
+            return node;
+        }
+
+        private BSTNode FindByRecursive(BSTNode node, int value)
+        {
+
+            if (node == null)
+            {
+                return null;
+            }
+            if (node.Value == value)
+            {
+                return node;
+            }
+            if (node.Value > value)
+            {
+                node = node.Left;
+            }
+            else
+            {
+                node = node.Right;
+            }
+            return FindByRecursive(node, value);
+        }
+
+        private BSTNode FindFatherOfNode(BSTNode father, BSTNode child, int value)
+        {
+            if (child == null)
+            {
+                return father;
+            }
+
+            if (value < father.Value)
+            {
+                if (father.Left.Value == value)
+                {
+                    return father;
+                }
+                father = father.Left;
+                child = child.Left;
+                return FindFatherOfNode(father, child, value);
+            }
+            else if (value > father.Value)
+            {
+                if (father.Right.Value == value)
+                {
+                    return father;
+                }
+                father = father.Right;
+                child = child.Right;
+                return FindFatherOfNode(father, child, value);
+            }
+            else
+            {
+                return father;
+            }
+
+        }
+
+        public bool DeleteItem(int value, BSTNode parent)
+        {
+            if (parent != null)
+            {
+                var node = FindItem(value);
+                if (node == null)
+                {
+                    return false;
+                }
+                var father = FindFatherOfNode(parent, node, value);
+                if (father != null)
+                {
+                    bool isSuccedd = false;
+                    if (node.Left != null)
+                    {
+                        father.Left = node.Left;
+                        isSuccedd = true;
+                    }
+                    else
+                    {
+                            father.Left = null;
+                            isSuccedd = true;
+                        
+                    }
+                    if (node.Right != null)
+                    {
+                        father.Right = node.Right;
+                        isSuccedd = true;
+                    }
+                    else 
+                    {
+                        father.Right = null;
+                        isSuccedd = true;
+                    }
+                    
+
+                return isSuccedd;
+            }
+            else
+            {
+                return false;
+            }
+        }
+            return false;
+            
+        }
+}
+}
